b609bb70d75317b23d971090f8923528ad271a9a
FED-uttam <fed.uttammate@gmail.com>
2025-12-04 21:41:54 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
+  <h2>JS Drum Kit</h2>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
