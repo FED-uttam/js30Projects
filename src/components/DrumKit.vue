050b5b9c5479ca8c98fe779a674c2a32b4515eb5
FED-uttam <fed.uttammate@gmail.com>
2025-12-05 16:21:47 +0530

    little finished project working

--- src/components/DrumKit.vue
+++ src/components/DrumKit.vue
@@ -1,34 +1,65 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 const keys = ref([
-  { "keyCode": 65, "keyChar": "A", "sound": "clap", "src": "sounds/clap.wav" },
-  { "keyCode": 83, "keyChar": "S", "sound": "hihat", "src": "sounds/hihat.wav" },
-  { "keyCode": 68, "keyChar": "D", "sound": "kick", "src": "sounds/kick.wav" },
-  { "keyCode": 70, "keyChar": "F", "sound": "openhat", "src": "sounds/openhat.wav" },
-  { "keyCode": 71, "keyChar": "G", "sound": "boom", "src": "sounds/boom.wav" },
-  { "keyCode": 72, "keyChar": "H", "sound": "ride", "src": "sounds/ride.wav" },
-  { "keyCode": 74, "keyChar": "J", "sound": "snare", "src": "sounds/snare.wav" },
-  { "keyCode": 75, "keyChar": "K", "sound": "tom", "src": "sounds/tom.wav" },
-  { "keyCode": 76, "keyChar": "L", "sound": "tink", "src": "sounds/tink.wav" }
+  { "keyCode": 65, "keyChar": "A", "sound": "clap", "src": "/JS-30-Projects/sounds/clap.wav" },
+  { "keyCode": 83, "keyChar": "S", "sound": "hihat", "src": "/JS-30-Projects/sounds/hihat.wav" },
+  { "keyCode": 68, "keyChar": "D", "sound": "kick", "src": "/JS-30-Projects/sounds/kick.wav" },
+  { "keyCode": 70, "keyChar": "F", "sound": "openhat", "src": "/JS-30-Projects/sounds/openhat.wav" },
+  { "keyCode": 71, "keyChar": "G", "sound": "boom", "src": "/JS-30-Projects/sounds/boom.wav" },
+  { "keyCode": 72, "keyChar": "H", "sound": "ride", "src": "/JS-30-Projects/sounds/ride.wav" },
+  { "keyCode": 74, "keyChar": "J", "sound": "snare", "src": "/JS-30-Projects/sounds/snare.wav" },
+  { "keyCode": 75, "keyChar": "K", "sound": "tom", "src": "/JS-30-Projects/sounds/tom.wav" },
+  { "keyCode": 76, "keyChar": "L", "sound": "tink", "src": "/JS-30-Projects/sounds/tink.wav" }
 ])
+
+const activeKey = ref(null)
+const playSound = (keyCode) => {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`)
+  if(!audio) return
+  audio.currentTime = 0;
+  audio.play();
+  activeKey.value = keyCode;
+
+  setTimeout(()=> {
+    if(activeKey.value === keyCode) {
+      activeKey.value = null
+    }
+  }, 150)
+
+}
+
+const handleKeyDown = (e) => {
+ playSound(e.keyCode);
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown)
+})
+
+onUnmounted(() => {
+window.removeEventListener('keydown', handleKeyDown)
+})
 </script>
 
 <template>
   <div class="main">
-    <h2>Drums</h2>
+    <h1>Drums</h1>
 
     <!-- Center container -->
     <div class="keys-container">
-      <div class="drumKeys" v-for="item in keys" :key="item.keyCode">
+      <div class="drumKeys" :class="{ playing: activeKey === item.keyCode}" v-for="item in keys" :key="item.keyCode" :data-key="item.keyCode">
         <kbd>{{ item.keyChar }}</kbd>
         <span class="soundName">{{ item.sound }}</span>
       </div>
     </div>
   </div>
-  <div class="audioFiles" v-for="key in keys" :key="key.keyCode">
-    <audio :src="key.src" />
-  </div>
+    <audio
+      v-for="key in keys"
+      :key="key.keyCode"
+      :data-key="key.keyCode"
+      :src="key.src"
+    ></audio>
 </template>
 
 
@@ -44,16 +75,22 @@ const keys = ref([
   align-items: center;    /* Centers keys horizontally */
   justify-content: center; /* Centers everything vertically */
 }
-
+h1 {
+  color: #ffc600;
+}
 .keys-container {
   display: flex;
   gap: 20px;               /* Space between drum keys */
   flex-wrap: wrap;
   justify-content: center; /* Center inside container */
+  transition: all .07s ease;
 }
-
+.playing {
+  transform: scale(1.2);
+  border-color: #e617b2;
+  box-shadow: 0 0 1rem #da1313;
+}
 .drumKeys {
-  border: 2px solid #ffcc00;
   padding: 20px;
   min-width: 80px;
   background: rgba(0,0,0,0.4);
